92c2ca10d4f8976aff956bd58dde2224287f59ae
sumitWebDev <dassumit.ece@gmail.com>
2024-06-05 00:44:30 -0600

    Added jobModels

---
+++ backend/models/jobModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+
+const jobSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        trim:true,
+        required: [true, 'Title is required'],
+        maxlength:70
+    },
+    description:{
+        type:String,
+        trim:true,
+        required: [true, 'Description is required']
+    },
+    salary:{
+        type:String,
+        trim:true,
+        required: [true, 'Salary is required']
+    },
+    location:{
+        type:String,
+    },
+    available:{
+        type:Boolean,
+        default: true
+    },
+},{timestamps:true})
+
+module.exports = mongoose.model("Job",jobSchema)
